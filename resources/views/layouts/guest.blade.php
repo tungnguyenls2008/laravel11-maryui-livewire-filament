76d9c6ce31c09a84805fea10284e59f32f215d4e
tungnguyen <tungnguyenls2008@gmail.com>
2024-12-10 16:55:32 +0700

    add vietnam locations

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -19,6 +19,7 @@
     </head>
     <body>
         <div class="font-sans text-gray-900 dark:text-gray-100 antialiased">
+
             {{ $slot }}
         </div>
 
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -21,6 +21,8 @@
     </head>
     <body class="font-sans antialiased dark:bg-black dark:text-white/50">
         <div class="bg-gray-50 text-black/50 dark:bg-black dark:text-white/50">
+            @include('guest-navigation-menu')
+
             <img id="background" class="absolute -left-20 top-0 max-w-[877px]" src="https://laravel.com/assets/img/welcome/background.svg" alt="Laravel background" />
 
         </div>
